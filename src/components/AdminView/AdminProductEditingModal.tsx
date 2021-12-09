c6d7e1f503a077bb3ac71858e43e6a49709d1e44
210234 Hanna Kraska <210234@edu.p.lodz.pl>
2021-12-10 00:37:14 +0100

    created and styled admin products view modals

--- src/components/AdminView/AdminProductEditingModal.tsx
+++ src/components/AdminView/AdminProductEditingModal.tsx
@@ -13,8 +13,15 @@ import {
    Center,
    Image,
    Text,
+   Stack,
+   Input,
+   InputGroup,
+   InputLeftAddon,
 } from '@chakra-ui/react'
 import { Product } from '../../types/ProductType'
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
+
+import { EditIcon, ChevronDownIcon } from '@chakra-ui/icons'
 
 interface ProductModalProps {
    product: Product
@@ -29,6 +36,9 @@ export const AdminProductEditingModal: React.FC<ProductModalProps> = ({
    }
    const { name, description, unitPrice, unitWeight, categories } = product
    const { isOpen, onOpen, onClose } = useDisclosure()
+   const bgImg =
+      "url('https://images.unsplash.com/photo-1530362502708-d02c8f093039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
+
    return (
       <>
          <Box
@@ -56,6 +66,7 @@ export const AdminProductEditingModal: React.FC<ProductModalProps> = ({
                borderRadius="xl"
                bg="rgba(42, 67, 101, 0.98)"
                bgGradient="linear(to-tr, pink.800, orange.900, blue.800)"
+               backgroundImage={bgImg}
             >
                <Flex direction="column">
                   <ModalHeader
@@ -92,9 +103,141 @@ export const AdminProductEditingModal: React.FC<ProductModalProps> = ({
                            <Text>{description} </Text>
                         </Box>
                      </Box>
+
+                     <Stack spacing={3}>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Name"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Input placeholder={name} />
+                        </InputGroup>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Weight"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Input placeholder={unitWeight} />
+                        </InputGroup>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Category"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Menu>
+                              <MenuButton
+                                 as={Button}
+                                 variant="outline"
+                                 rightIcon={<ChevronDownIcon />}
+                              >
+                                 {categories}
+                              </MenuButton>
+                              <MenuList
+                                 bg="blue.500"
+                                 color="black"
+                                 borderRadius="xl"
+                              >
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Ingredients
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Equipment
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Clothing
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Materials
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Collectibles
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Perishable
+                                 </MenuItem>
+                                 <MenuItem
+                                    _hover={{
+                                       backgroundColor: 'pink.500',
+                                       color: 'white',
+                                    }}
+                                 >
+                                    Non-perishable
+                                 </MenuItem>
+                              </MenuList>
+                           </Menu>
+                        </InputGroup>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Description"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Input placeholder={description} />
+                        </InputGroup>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Price"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Input placeholder={unitPrice} />
+                        </InputGroup>
+                        <InputGroup>
+                           <InputLeftAddon
+                              children="Image"
+                              size="xl"
+                              bg="none"
+                           />
+                           <Input placeholder="URL" />
+                        </InputGroup>
+                     </Stack>
                   </ModalBody>
 
                   <ModalFooter>
+                     <Box
+                        borderWidth="5px"
+                        borderColor="pink.800"
+                        borderRadius="xl"
+                        bg="pink.300"
+                        mr={3}
+                     >
+                        <Button onClick={onClose} variant="magic-navbar">
+                           Save changes
+                        </Button>
+                     </Box>
                      <Box
                         borderWidth="5px"
                         borderColor="pink.800"
